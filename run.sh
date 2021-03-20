23490bf5e809d5bf93e93bdc96c8bda9e82c36df
lsiddd <lucassidpacheco@gmail.com>
2021-03-20 12:14:27 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 num_uav=10
 num_bs=20
+simul_time=30
 
 if [ $# -ne 0 ]
 then
@@ -18,16 +19,19 @@ then
     num_uav=0
 fi
 
+echo $algorithm
+
 sed -i -r "s/(numUAVs =) [0-9]{1,2}/\1 ${num_uav}/g" scratch/drones_and_stuff.cc
 sed -i -r "s/(numStaticCells =) [0-9]{1,2}/\1 ${num_bs}/g" scratch/drones_and_stuff.cc
 sed -i -r "s/(handover_policy =) \"\w+\"/\1 \"${algorithm}\"/g" scratch/drones_and_stuff.cc
+sed -i -r "s/(SimTime =) [0-9]{1,2}/\1 ${simul_time}/g" scratch/drones_and_stuff.cc
 
-for num_ue in 30 60 90 120
+for num_ue in 30  60 90 120
 do
-    for seed in $(seq 4)
+    for seed in $(seq 5)
     do
         sed -i -r "s/(numUes =) [0-9]{1,2}/\1 ${num_ue}/g" scratch/drones_and_stuff.cc
-        
+        echo "Simulation ${num_ue} users and random seed ${seed}."
         ./waf --run "scratch/drones_and_stuff --seedValue=$seed" > simul_${algorithm}_${seed}_${num_ue} 2>&1
     done
 done
